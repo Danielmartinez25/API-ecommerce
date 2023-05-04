fbcf320ade8dddc123e06c5681f398e21bd2d715
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-04 12:27:35 -0300

    route user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { } = require('../controller/userController')
+router
+  .get("/user/:id",)
+  .put("/update/:id",)
+  .post("/login",)
+  .post("/register",)
+  .delete("/remove/:id",);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 module.exports = router;
