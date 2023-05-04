a72aeb9326da2f8608ed9f4c8efe197c4df65698
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-04 16:40:37 -0300

    routes and controller user

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,42 @@
 module.exports = {
-    
-}
+  detail: async () => {
+    try {
+    } catch (error) {}
+  },
+  allUser: async () => {
+    try {
+    } catch (error) {}
+  },
+  login: async () => {
+    try {
+    } catch (error) {}
+  },
+  register: async () => {
+    try {
+    } catch (error) {}
+  },
+  remove: async () => {
+    try {
+    } catch (error) {}
+  },
+  update: async () => {
+    try {
+    } catch (error) {}
+  },
+  checked: async () => {
+    try {
+    } catch (error) {}
+  },
+  sendToken: async () => {
+    try {
+    } catch (error) {}
+  },
+  verifyToken: async () => {
+    try {
+    } catch (error) {}
+  },
+  changePassword: async () => {
+    try {
+    } catch (error) {}
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { } = require('../controller/userController')
+const {detail,allUser,update,remove,login,register,changePassword,checked,sendToken,verifyToken} = require('../controller/userController')
 router
-  .get("/user/:id",)
-  .put("/update/:id",)
-  .post("/login",)
-  .post("/register",)
-  .delete("/remove/:id",);
+  .get("/user/:id", detail)
+  .get("/users", allUser)
+  .put("/update/:id", update)
+  .post("/login", login)
+  .post("/register", register)
+  .delete("/remove/:id", remove)
+    .get('/checked',checked)
+    .post('/send-token',sendToken)
+    .route('/reset-password')
+        .get(verifyToken)
+        .post(changePassword)
 
 
 module.exports = router;
