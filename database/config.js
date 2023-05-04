34142ee1c9847b1cd223d4c74195c52c1855c056
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-04 11:17:16 -0300

    variables de entorno

--- database/config.js
+++ database/config.js
@@ -1,1 +1,9 @@
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
+
+const mongoDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.DB_CONNECTION)
+    } catch (error) {
+        
+    }
+}
