75731d8c2332c64851a213f064b18051c4d5310a
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-08 18:22:08 -0300

    fileupload middleware

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const fileUpload = require('express-fileupload')
 const mongoDB = require('./database/config')
 mongoDB()
 
@@ -17,12 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "./uploads",
-  })
-);
 app.use("/api/users", usersRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const fileUpload = require("express-fileupload");
 const {detail,allUser,update,remove,login,register,changePassword,checked,sendToken,verifyToken, removeAll} = require('../controller/userController')
 router
   .get("/user/:id", detail)
   .get("/users", allUser)
   .put("/update/:id", update)
   .post("/login", login)
-  .post("/register", register)
+  .post(
+    "/register",fileUpload({useTempFiles: true,tempFileDir: "./uploads",}),register)
   .delete("/remove/:id", remove)
-  .delete('/remove',removeAll)
-    .get('/checked',checked)
-    .post('/send-token',sendToken)
-    .route('/reset-password')
-        .get(verifyToken)
-        .post(changePassword)
+  .delete("/remove", removeAll)
+  .get("/checked", checked)
+  .post("/send-token", sendToken)
+  .route("/reset-password")
+  .get(verifyToken)
+  .post(changePassword);
 
 
 module.exports = router;
