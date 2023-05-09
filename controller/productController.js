2ca7b5dfc20a452cf53a7894db0768cfb5304699
Danimartinez25 <danimartinez1325@gmail.com>
2023-05-09 20:01:09 -0300

    create routes and controller

--- controller/productController.js
+++ controller/productController.js
@@ -1,2 +1,8 @@
 module.exports = {
-}
+  create: async (req, res) => {},
+  update: async (req, res) => {},
+  detail: async (req, res) => {},
+  listProduct: async (req, res) => {},
+  comment: async (req, res) => {},
+  remove: async (req, res) => {},
+};
